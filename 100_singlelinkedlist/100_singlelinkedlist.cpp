4ec3637e23a606ba2722652576681950a88653f1
riqsans <thrq88@gmail.com>
2024-04-26 14:08:49 +0700

    penyisipan node terurut

--- 100_singlelinkedlist/100_singlelinkedlist.cpp
+++ 100_singlelinkedlist/100_singlelinkedlist.cpp
@@ -20,11 +20,32 @@ void addNode() {
 	nodeBaru->noMhs = nim;
 	nodeBaru->name = nama;
 
-	if (START == NULL || nime <= START->noMhs) {
-		if (START != NULL && nime == START->noMHS)
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs)
 		{
 			cout << "NIM sudah ada" << endl;
 			return;
 		}
+
+		nodeBaru->next = START;
+		START = nodeBaru;
+		return;
 	}
+
+	Node* previous = START;
+	Node* current = START;
+
+	while ((current != NULL) && (nim >= current->noMhs))
+	{
+		if (nim == current->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+		previous = current;
+		current = previous;
+	}
+
+	nodeBaru->next = current;
+	previous->next = nodeBaru;
 }
